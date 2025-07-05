75b5203033871a768dafa4277e248b75f2101c64
Basu <basudevkumar.india2019@gmail.com>
2025-07-06 01:17:30 +0530

    imageUrl bug fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -21,7 +21,9 @@ router.post("/upload-image",upload.single("image") , (req,res)=>{
         return res.status(400).json({message:"No file uploaded"});
     }
 
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    // const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    const imageUrl = `${process.env.BASE_URL}/uploads/${req.file.filename}`;
+
 
     res.status(200).json({ imageUrl });
 
