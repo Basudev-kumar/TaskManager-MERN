6ade0f59d19c93614c947d6c2e11f9f41302422e
Basu <basudevkumar.india2019@gmail.com>
2025-07-06 02:53:01 +0530

    add vercel.json

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -23,6 +23,7 @@ router.post("/upload-image",upload.single("image") , (req,res)=>{
 
     // const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
     const imageUrl = `${process.env.BASE_URL}/uploads/${req.file.filename}`;
+    
 
 
     res.status(200).json({ imageUrl });
