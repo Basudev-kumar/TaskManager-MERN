0d15bdcc0a18de31dee60820086ed2c8b8b26084
Basudev kumar <basudevkumar.india2019@gmail.com>
2025-08-22 14:17:30 +0530

    bug fix

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 // Configure storage
 const storage = multer.diskStorage({
 
-    destination: (req,res,cb) => {
+    destination: (req,file,cb) => {
         cb(null,'uploads/');
     },
 
